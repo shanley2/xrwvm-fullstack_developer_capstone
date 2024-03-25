6025c4b16d3d01f36779f6ac268dc6b4b52a288c
shanley2 <shanley907@gmail.com>
2024-03-25 16:31:07 -0400

    Linting part seven

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                'https://shanley907-8000.theiadockernext-0-labs-prod'
-                    '-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://shanley907-8000.theiadockernext-0-labs-prod'
+                 '-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://shanley907-8000.theiadockernext-0'
-    '-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                        '-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -97,7 +97,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.' \
+        'django.contrib.auth.password_validation.'
         'UserAttributeSimilarityValidator',
     },
     {
