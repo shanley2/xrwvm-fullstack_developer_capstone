3dffa0615dc18e5895320808cc930063d5147361
shanley2 <shanley907@gmail.com>
2024-03-25 16:24:51 -0400

    Linting part six

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -127,7 +127,7 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401,
-                                    "message": "Error in posting review"})
+                                "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
@@ -141,5 +141,5 @@ def get_cars(request):
     cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name,
-                        "CarMake": car_model.car_make.name})
+                    "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                    'https://shanley907-8000.theiadockernext-0-labs-prod'
+                'https://shanley907-8000.theiadockernext-0-labs-prod'
                     '-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://shanley907-8000.theiadockernext-0'
     '-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
