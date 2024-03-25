657089f70d60684a15fabc2f4bd1a112a32245e0
shanley2 <shanley907@gmail.com>
2024-03-25 16:18:55 -0400

    Linting part five

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,8 +29,9 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-    'https://shanley907-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://shanley907-8000.theiadockernext-0' \
+                    'https://shanley907-8000.theiadockernext-0-labs-prod'
+                    '-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://shanley907-8000.theiadockernext-0'
     '-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
