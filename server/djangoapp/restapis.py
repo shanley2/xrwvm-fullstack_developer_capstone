b64c1b7a9683bf9ac513a40e0ee1fa3b2dc8d5e0
shanley2 <shanley907@gmail.com>
2024-03-25 15:34:22 -0400

    Linting part one

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,11 +11,12 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -46,4 +47,3 @@ def post_review(data_dict):
         return response.json()
     except:
         print("Network exception occurred")
-
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
     # path for add a review view
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
