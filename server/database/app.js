13464b28674b0f3dbc580f5335856a646af4d2fa
shanley2 <shanley907@gmail.com>
2024-03-25 14:52:06 -0400

    Week 4 completed

--- server/database/app.js
+++ server/database/app.js
@@ -79,7 +79,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
     try {
-        const documents = await Reviews.find({id: req.params.id});
+        const documents = await Dealerships.find({id: req.params.id});
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path(route='register', view=views.registration, name='register'),
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='get_dealers/<int:id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     # path for dealer reviews view
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     path(route='add_review', view=views.add_review, name='add_review'),
